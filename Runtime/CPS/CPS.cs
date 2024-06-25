f843e7f1b6c521ca3de374f005a90f6c2af6718a
Eloi Stree <eloistree@gmail.com>
2024-06-25 23:14:12 +0200

    Save: 06/25/2024 11:14 PM

---
+++ Runtime/CPS/CPS.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPS 
+{
+    
+    public static CPS_DroneSoccerBallGoals CPS_DroneSoccerBallGoals = new CPS_DroneSoccerBallGoals();
+    public static CPS_DroneSoccerBallPosition CPS_DroneSoccerBallPosition = new CPS_DroneSoccerBallPosition();
+    public static CPS_DroneSoccerIndexIntegerClaim CPS_DroneSoccerIndexIntegerClaim = new CPS_DroneSoccerIndexIntegerClaim();
+    public static CPS_DroneSoccerMatchState CPS_DroneSoccerMatchState = new CPS_DroneSoccerMatchState();
+    public static CPS_DroneSoccerMatchStaticInformation CPS_DroneSoccerMatchStaticInformation = new CPS_DroneSoccerMatchStaticInformation();
+    public static CPS_DroneSoccerPositions CPS_DroneSoccerPositions = new CPS_DroneSoccerPositions();
+    public static CPS_DroneSoccerPublicXmlRsaKey1024Claim CPS_DroneSoccerPublicXmlRsaKey1024Claim = new CPS_DroneSoccerPublicXmlRsaKey1024Claim();
+    public static CPS_DroneSoccerTimeValue CPS_DroneSoccerTimeValue = new CPS_DroneSoccerTimeValue();
+    public static CPS_LinearProjectilePoolItemCreationEvent CPS_LinearProjectilePoolItemCreationEvent = new CPS_LinearProjectilePoolItemCreationEvent();
+    public static CPS_NetworkGameFramePushTiming CPS_NetworkGameFramePushTiming = new CPS_NetworkGameFramePushTiming();
+    public static CPS_PoolItemDestructionEvent CPS_PoolItemDestructionEvent = new CPS_PoolItemDestructionEvent();
+}
